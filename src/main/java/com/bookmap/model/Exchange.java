c6ce2ccee6b8d7599ef9d2c2543b08c668710942
Alex <oleksandr.paseka@ticktech.bg>
2019-12-04 17:02:49 +0200

    changed logic of exchange due to new interface implementation. added test

--- src/main/java/com/bookmap/model/Exchange.java
+++ src/main/java/com/bookmap/model/Exchange.java
@@ -3,37 +3,48 @@ package com.bookmap.model;
 import com.bookmap.internal.RequestRejectedException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-public class Exchange implements QueryInterface, ExchangeInterface {
+public class Exchange implements QueryInterface, AdvancedExchangeInterface {
 
-    private List<Order> restingOrders = new ArrayList<>();
+    private List<Order> restingBuyOrders = new ArrayList<>();
+    private List<Order> restingSellOrders = new ArrayList<>();
 
-    public List<Order> getRestingOrders() {
-        return restingOrders;
+    public List<Order> getRestingBuyOrders() {
+        return restingBuyOrders;
+    }
+
+    public List<Order> getRestingSellOrders() {
+        return restingSellOrders;
+    }
+
+    private List<Order> getSort(List<Order> orders) {
+        Collections.sort(orders);
+        return orders;
     }
 
     @Override
     public void send(long orderId, boolean isBuy, int price, int size) throws RequestRejectedException {
         Order order = new Order(orderId, isBuy, price, size);
-        List<Order> sortedOrders;
         if (order.isBuy()) {
-            sortedOrders = restingOrders.stream().filter(o -> !o.isBuy() && o.getPrice() <= order.getPrice()).sorted((o1, o2) -> o1.compare(o1, o2)).collect(Collectors.toList());
+            List<Order> appropriateOrders = getSort(restingSellOrders).stream().filter(o -> o.getPrice() <= order.getPrice()).collect(Collectors.toList());
+            matchOrders(order, appropriateOrders, restingBuyOrders, restingSellOrders);
         } else {
-            sortedOrders = restingOrders.stream().filter(o -> o.isBuy() && o.getPrice() >= order.getPrice()).sorted((o1, o2) -> o1.compare(o1, o2)).collect(Collectors.toList());
+            List<Order> appropriateOrders = getSort(restingBuyOrders).stream().filter(o -> o.getPrice() >= order.getPrice()).collect(Collectors.toList());
+            matchOrders(order, appropriateOrders, restingSellOrders, restingBuyOrders);
         }
-        matchOrders(order, sortedOrders);
     }
 
-    private void matchOrders(Order order, List<Order> sortedOrders) {
+    private void matchOrders(Order order, List<Order> appropriateOrders, List<Order> restingOrdersIn, List<Order> restingOrdersOut) {
         Order nextOrder = null;
-        while (order.getSize() > 0 && sortedOrders.iterator().hasNext()) {
-            nextOrder = sortedOrders.iterator().next();
+        while (order.getSize() > 0 && appropriateOrders.iterator().hasNext()) {
+            nextOrder = appropriateOrders.iterator().next();
             if (nextOrder.getSize() <= order.getSize()) {
-                restingOrders.remove(nextOrder);
-                sortedOrders.remove(nextOrder);
+                restingOrdersOut.remove(nextOrder);
+                appropriateOrders.remove(nextOrder);
             }
             order.setSize(order.getSize() - nextOrder.getSize());
         }
@@ -41,13 +52,13 @@ public class Exchange implements QueryInterface, ExchangeInterface {
             nextOrder.setSize(-order.getSize());
         }
         if (order.getSize() > 0) {
-            restingOrders.add(order);
+            restingOrdersIn.add(order);
         }
     }
 
     @Override
     public void cancel(long orderId) throws RequestRejectedException {
-        if (!restingOrders.removeIf(order -> order.getId() == orderId)) {
+        if (!restingBuyOrders.removeIf(order -> order.getId() == orderId) && !restingSellOrders.removeIf(order -> order.getId() == orderId)) {
             throw new RequestRejectedException();
         }
     }
@@ -55,20 +66,44 @@ public class Exchange implements QueryInterface, ExchangeInterface {
     @Override
     public int getTotalSizeAtPrice(int price) {
         int size = 0;
-        for (Order order : restingOrders) {
+        for (Order order : mergedLists()) {
             if (order.getPrice() == price)
                 size = size + order.getSize();
         }
         return size;
     }
 
+    private List<Order> mergedLists() {
+        List<Order> mergedSet = new ArrayList<>();
+        mergedSet.addAll(restingBuyOrders);
+        mergedSet.addAll(restingSellOrders);
+        return mergedSet;
+    }
+
     @Override
     public int getHighestBuyPrice() {
-        return restingOrders.stream().filter(Order::isBuy).mapToInt(Order::getPrice).max().orElseThrow(NoSuchElementException::new);
+        return restingBuyOrders.stream().mapToInt(Order::getPrice).max().orElseThrow(NoSuchElementException::new);
     }
 
     @Override
     public int getLowestSellPrice() {
-        return restingOrders.stream().filter(o -> !o.isBuy()).mapToInt(Order::getPrice).min().orElseThrow(NoSuchElementException::new);
+        return restingSellOrders.stream().mapToInt(Order::getPrice).min().orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public void modify(long oid, int newPrice, int newSize, boolean keepPositionOnSizeDecrease) throws RequestRejectedException {
+        Order order = mergedLists().stream().filter(o -> o.getId() == oid).findAny().orElseThrow(RequestRejectedException::new);
+        if (keepPositionOnSizeDecrease && newPrice == order.getPrice() && newSize < order.getSize()) {
+            order.setSize(newSize);
+        } else {
+            Order modifiedOrder = new Order(oid, order.isBuy(), newPrice, newSize);
+            if (order.isBuy()) {
+                restingBuyOrders.remove(order);
+                restingBuyOrders.add(modifiedOrder);
+            } else {
+                restingSellOrders.remove(order);
+                restingSellOrders.add(modifiedOrder);
+            }
+        }
     }
 }
